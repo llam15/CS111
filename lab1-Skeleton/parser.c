d07f3efb506fb391552bc8469bd0faf85fc3719b
llam15 <leslielam@ucla.edu>
2015-01-14 14:38:24 -0800

    Now supports: Basic pipes: A|B or A|B|C|D, etc Basic pipe in if statement: if A|B; then C; fi

--- lab1-Skeleton/parser.c
+++ lab1-Skeleton/parser.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define DEFAULT_WORD_NUM (4)
 
@@ -13,6 +14,8 @@ static Token_t* g_tok_list;
 static uint64_t g_tok_index;
 static uint64_t g_tok_list_len;
 static char* g_tok_buffer;
+static bool simple_word;
+static int if_depth;
 
 // A tree. Used to store the head of a recursively-traversed section of the command tree
 typedef struct
@@ -27,6 +30,8 @@ static uint64_t line_num;
 // Parse a token list and store the generated command tree into parsed_commands->command_tree
 void parse(Token_t* tok_list, char* tok_buffer, uint64_t tok_list_len, command_t* ret_tree)
 {
+    if_depth = 0;
+    simple_word = false;
     g_tok_buffer = tok_buffer;
     g_tok_index = 0;
     g_tok_list = tok_list;
@@ -37,9 +42,9 @@ void parse(Token_t* tok_list, char* tok_buffer, uint64_t tok_list_len, command_t
 // Moves the read index forward, and returns true on end of token list
 bool getTok(void)
 {
-    if (g_tok_index == g_tok_list_len)
+  //** CHANGED G_TOK_LIST_LEN TO G_TOK_LIST_LEN-1
+    if (g_tok_index++ >= g_tok_list_len-1)
         return true;
-    g_tok_index++;
     return false;
 }
 
@@ -76,7 +81,7 @@ int8_t insert_child(command_t parent, command_t child)
 
     if (num_children == 0)
     {
-        fprintf(stderr, "%"PRIu64": Syntax Error: Token nesting not supported for command num %d\n", line_num, parent->type);
+      fprintf(stderr, "%"PRIu64": Syntax Error: Token nesting not supported for command num %d\n", line_num, parent->type);
         return -1;
     }
 
@@ -158,10 +163,11 @@ command_t construct_node(command_type type)
     return ret;
 }
 
+
 // Note: returns the newly created node
 uint8_t insert_node(command_type type, tree_context * context, command_t* ret_command)
 {
-    int8_t rval = 0;
+//    int8_t rval = 0;
 
     command_t node = construct_node(type);
 
@@ -172,26 +178,27 @@ uint8_t insert_node(command_type type, tree_context * context, command_t* ret_co
     {
 
       //** CHANGED CONTEXT->CUR_NODE TO CONTEXT->ROOT
-        rval = insert_child(context->root, node);
-        if(rval < 0)
-            exit(rval);
+        context->root->child_index = insert_child(context->root, node);
+        if(context->root->child_index < 0)
+            exit(context->root->child_index);
 
     }
 
     context->cur_node = node;
     *ret_command = node;
-    return rval;
+    return context->root->child_index;
 }
 
 void shell_inner(tree_context * context)
 {
-    printf("shell_inner: tree_context =\t%"PRIu64"\t%"PRIu64"\n", (uint64_t)context, (uint64_t)context->root);
-    static uint8_t insertion_index = 0;
-    while(!getTok())
+    //static
+
+    do
     {
         // Create a new tree context to hold the branch
         tree_context inner_context;
-        command_t temp_root = NULL;
+//        uint8_t insertion_index = 0;
+        command_t temp_node = NULL;
 	inner_context.root = inner_context.cur_node = NULL;
 
         // Update the line number
@@ -199,18 +206,20 @@ void shell_inner(tree_context * context)
 
         // If the parent node is a SIMPLE_COMMAND
 	//** ADDED CHECK FOR NULL
-        if(context->root != NULL && context->root->type == SIMPLE_COMMAND)
+        if(simple_word)
         {
             // Only a few special tokens will break out of the SIMPLE_COMMAND argument list
             switch(g_tok_list[g_tok_index].type)
             {
             case TOK_SC:
             case TOK_PIPE:
+            case TOK_NL:
             case TOK_LPAREN:
             case TOK_RPAREN:
             case TOK_LAB:
             case TOK_RAB:
-            case TOK_NL:
+                simple_word = false;
+                g_tok_index--;
                 return;
                 break;
             default:
@@ -226,28 +235,30 @@ void shell_inner(tree_context * context)
             switch (g_tok_list[g_tok_index].type)
             {
             case TOK_IF:
-	      //**CHANGED SUBSHELL->IF
+                //**CHANGED SUBSHELL->IF
                 // Add the new node to the supertree; maintain insertion index
-
-                insertion_index = insert_node(IF_COMMAND, context, &inner_context.root);
+                if_depth++;
+                //insertion_index =
+                    insert_node(IF_COMMAND, context, &inner_context.root);
 
                 inner_context.cur_node = inner_context.root;
 
                 // Call shell_inner() recursively, passing the subtree context
-		if(getTok())
-		  return;
-		shell_inner(&inner_context);
+                if(getTok())
+                  return;
+                shell_inner(&inner_context);
                 break;
             case TOK_THEN:
                 // If the root (parent) node type is not IF_COMMAND, or if a TOK_THEN has already been seen
-                if(context->root->type != IF_COMMAND || insertion_index != 0)
+                if(context->root->type != IF_COMMAND || context->root->child_index != 0)//insertion_index != 0)
                 {
                     fprintf(stderr, "%"PRIu64": Syntax error: unexpected `then'\n", line_num);
                     exit(-1);
                 }
                 break;
             case TOK_FI:
-                if(context->root->type != IF_COMMAND || insertion_index == 0)
+                if_depth--;
+                if(context->root->type != IF_COMMAND || context->root->child_index == 0)//insertion_index == 0)
                 {
                     fprintf(stderr, "%"PRIu64": Syntax error: unexpected `fi'\n", line_num);
                     exit(-1);
@@ -257,40 +268,43 @@ void shell_inner(tree_context * context)
                 break;
             case TOK_ELSE:
                 // If the root (parent) node type is not IF_COMMAND, or if a TOK_THEN has already been seen
-                if(context->root->type != IF_COMMAND || insertion_index != 1)
+                if(context->root->type != IF_COMMAND || context->root->child_index != 1)//insertion_index != 1)
                 {
-                    fprintf(stderr, "%"PRIu64": Syntax error: unexpected `then'\n", line_num);
+                    fprintf(stderr, "%"PRIu64": Syntax error: unexpected `else'\n", line_num);
                     exit(-1);
                 }
                 break;
             case TOK_WHILE:
                 // Add the new node to the supertree; maintain insertion index
-                insertion_index = insert_node(WHILE_COMMAND, context, &inner_context.root);
+//                insertion_index =
+                    insert_node(WHILE_COMMAND, context, &inner_context.root);
                 inner_context.cur_node = inner_context.root;
 
                 // Call shell_inner() recursively, passing the subtree context
-		if(getTok())
-		  return;
+                if(getTok())
+                  return;
                 shell_inner(&inner_context);
                 break;
             case TOK_UNTIL:
                 // Add the new node to the supertree; maintain insertion index
-                insertion_index = insert_node(UNTIL_COMMAND, context, &inner_context.root);
+//                insertion_index =
+                    insert_node(UNTIL_COMMAND, context, &inner_context.root);
                 inner_context.cur_node = inner_context.root;
 
                 // Call shell_inner() recursively, passing the subtree context
-		if(getTok())
-		  return;
+                if(getTok())
+                  return;
                 shell_inner(&inner_context);
                 break;
             case TOK_DO:
-                if(context->root->type != WHILE_COMMAND || context->root->type != UNTIL_COMMAND || insertion_index != 0)
+                if(context->root->type != WHILE_COMMAND || context->root->type != UNTIL_COMMAND || context->root->child_index != 0)//insertion_index != 0)
                 {
                     fprintf(stderr, "%"PRIu64": Syntax error: unexpected `do'\n", line_num);
+                    exit(-1);
                 }
                 break;
             case TOK_DONE:
-                if(context->root->type != WHILE_COMMAND || context->root->type != UNTIL_COMMAND || insertion_index != 1)
+                if(context->root->type != WHILE_COMMAND || context->root->type != UNTIL_COMMAND || context->root->child_index != 1)//insertion_index != 1)
                 {
                     fprintf(stderr, "%"PRIu64": Syntax error: unexpected `done'\n", line_num);
                     exit(-1);
@@ -300,53 +314,68 @@ void shell_inner(tree_context * context)
                 break;
             case TOK_WORD:
                 // Add the new node to the supertree; maintain insertion index
-                insertion_index = insert_node(SIMPLE_COMMAND, context, &inner_context.root);
+//                insertion_index =
+                    insert_node(SIMPLE_COMMAND, context, &inner_context.root);
                 inner_context.cur_node = inner_context.root;
-
+                simple_word = true;
                 // Call shell_inner() recursively, passing the subtree context
-		if(getTok())
-		  return;
+                if(getTok())
+                  return;
                 shell_inner(&inner_context);
                 break;
             case TOK_SC:
-            case TOK_PIPE:
             case TOK_NL:
-	        //** IF LAST TOKEN, DON'T SPLIT!
-	        if(g_tok_index == g_tok_list_len-1)
-		  return;
-
+                    if (if_depth > 0) {
+                        return;
+                    }
+            case TOK_PIPE:
+                //** IF LAST TOKEN, DON'T SPLIT!
+                if(g_tok_index == g_tok_list_len-1)
+                  return;
+	      
                 // Store the old root temporarily so tree can be reordered
-                temp_root = context->root;
+                temp_node = context->cur_node;
 
-                context->root = context->cur_node = NULL;
+                if (context->root->type == SIMPLE_COMMAND){
+                    temp_node = context->root;
+                    context->root = context->cur_node = NULL;
+                }
+                else
+                    context->root->u.command[context->root->child_index]= NULL;
+                
                 char* str_type = "Unknown linking";
                 if(g_tok_list[g_tok_index].type == TOK_SC)
                 {
                     str_type = "Sequence";
-                    insertion_index = insert_node(SEQUENCE_COMMAND, context, &inner_context.root);
+//                    insertion_index =
+                    insert_node(SEQUENCE_COMMAND, context, &inner_context.root);
                 }
                 if(g_tok_list[g_tok_index].type == TOK_PIPE)
                 {
                     str_type = "Pipe";
-                    insertion_index = insert_node(PIPE_COMMAND, context, &inner_context.root);
+//                    insertion_index =
+                    insert_node(PIPE_COMMAND, context, &inner_context.root);
                 }
+                    
+                //THIS WILL BE REMOVED AFTER FIX TOKENIZER
                 if(g_tok_list[g_tok_index].type == TOK_NL)
                 {
                     str_type = "New line";
-                    insertion_index = insert_node(SEQUENCE_COMMAND, context, &inner_context.root);
+//                    insertion_index =
+                    insert_node(SEQUENCE_COMMAND, context, &inner_context.root);
                 }
                 inner_context.cur_node = inner_context.root;
 
-                // Insert the whole tree that used to be located at context->root (now at temp_root) under the new sequence command.
-		// Should end up being inserted at the zeroth index.
-                if(insert_child(inner_context.root, temp_root) != 0)
+                // Insert the whole tree that used to be located at context->root (now at temp_root) under the new sequence command. 
+                // Should end up being inserted at the zeroth index.
+                if(insert_child(inner_context.cur_node, temp_node) != 0)
                 {
                     fprintf(stderr, "%"PRIu64": Internal error: %s token has incorrect number of children.\n", line_num, str_type);
                 }
 
                 // Call shell_inner() recursively, passing the subtree context
-		if(getTok())
-		  return;
+                if(getTok())
+                  return;
                 shell_inner(&inner_context);
                 break;
             case TOK_LPAREN:
@@ -366,21 +395,22 @@ void shell_inner(tree_context * context)
                 break;
             }
         }
-    }
+//        if (g_tok_index >= g_tok_list_len-1) {
+//            context->root = inner_context.root;
+//        }
+    } while(!getTok());
 }
 
 command_t shell()
 {
     // Create an initial context; this is the root of all trees in the recursive structure
     tree_context* initial_context = (tree_context*)checked_malloc(sizeof(tree_context));
-    initial_context->cur_node = initial_context->root = construct_node(SUBSHELL_COMMAND);
+    initial_context->cur_node = initial_context->root = NULL;
 
     // Call shell_inner
     shell_inner(initial_context);
 
-    command_t ret = initial_context->root->u.command[0];
-
-    free(initial_context->root);
-
-    return ret;
+//    command_t ret = initial_context->root->u.command[0];
+//    free(initial_context->root);
+    return initial_context->root;
 }
