053d5fded0f24c419471c82b282bee1016b90f7b
fughilli <kbalke@ucla.edu>
2015-01-30 23:50:31 -0800

    Changed test-ok script for 1b to be of the same form as the 1a test script

--- Lab1/test-t-1b-ok.sh
+++ Lab1/test-t-1b-ok.sh
@@ -1,3 +1,29 @@
+#! /bin/sh
+
+# UCLA CS 111 Lab 1 - Test that valid syntax is processed correctly.
+
+# Copyright 2012-2014 Paul Eggert, Kevin Balke, Leslie Lam.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+tmp=$0-$$.tmp
+mkdir "$tmp" || exit
+
+(
+cd "$tmp" || exit
+
+cat >test.sh <<'EOF'
 #!/bin/sh
 
 # This comment should be ignored
@@ -10,8 +36,7 @@ echo hello > hello.txt #outputs to hello.txt
 
 cat < hello.txt > hello2.txt # outputs contents of hello.txt to hello2.txt
 
-echo hello world | less #outputs to less
-
+echo hello world | cat #outputs to cat
 
 echo hello word > file
 
@@ -23,3 +48,24 @@ until cat file2; do touch file2; done
 (echo I am a subshell)
 
 rm hello.txt hello2.txt file2
+EOF
+
+cat >test.exp <<'EOF'
+hello
+world
+hello world
+hello word
+I am a subshell
+EOF
+
+../profsh test.sh >test.out 2>test.err || exit
+
+diff -u test.exp test.out || exit
+test ! -s test.err || {
+  cat test.err
+  exit 1
+}
+
+) || exit
+
+rm -fr "$tmp"
