ef6fa2dd02f2a7526f2154c74a502fa50573a489
llam15 <leslielam@ucla.edu>
2015-02-13 01:40:30 -0800

    Fixed some errors in deadlock.sh

--- RAMDisk/deadlock.sh
+++ RAMDisk/deadlock.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo 'aaa' | osprdaccess -w 3 -l -d 0.2 &; sleep 0.1 && echo 'aaa' | osprdaccess -w 3 -l
+echo 'aaa' | ./osprdaccess -w 3 -l -d 0.2 & 
+sleep 0.1 && echo 'bbb' | ./osprdaccess -w 3 -l
